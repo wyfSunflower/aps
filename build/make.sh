fd36540a6b24cfeba2e85536ae68cd539fb34379
Si <miaosi2018@gmail.com>
2022-09-22 13:00:57 +0800

    rm *.lnk

--- build/make.sh
+++ build/make.sh
@@ -3,6 +3,6 @@ protoc --cpp_out=../protos --proto_path=../protos/ ../protos/global.proto
 if [[ $1 == "MinGW" ]]; then
    cmake .. -G "MinGW Makefiles"
 else
-   cmake ..
+   cmake .. -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_C_COMPILER=$(which gcc)
 fi
 make -j32
